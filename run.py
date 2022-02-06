b62c45310264cc61671493997068e959b522a0e7
Vlevinski <v.levinski@gmail.com>
2022-02-06 08:24:51 +0200

    Merge remote-tracking branch 'github/master'

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +8,9 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route('/list')
+def react():
+    return render_template('list.html')
 
 if __name__ == '__main__':
     app.run()
