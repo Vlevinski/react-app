3871f2a75f116a990dd186bfccc28b51f7b429d2
Vlevinski <v.levinski@gmail.com>
2022-02-09 12:13:50 +0200

    updates

--- run.py
+++ run.py
@@ -4,13 +4,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
+def home():
+    """ home page """
     return render_template('index.html')
 
-@app.route('/list')
-def react():
-    return render_template('list.html')
+
+@app.route('/hello')
+def hello():
+    """ hello example"""
+    return render_template('hello.html')
+
+
+@app.route('/timer')
+def timer():
+    """ timer example """
+    return render_template('timer.html')
+
+
+@app.route('/todos')
+def todos():
+    """ todos application """
+    return render_template('todos.html')
+
 
 if __name__ == '__main__':
     app.run()
